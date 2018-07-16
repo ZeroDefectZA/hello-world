c2a84f5a82307433f314ce9d38d312557aae9bee
Kumaran Naicker <kumaran.naicker@gmail.com>
2018-07-16 23:27:27 +0200

    adding course

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//#include <iostream>
+
+void main(){
+        printf("Hello World using printf. C");
+        //std::cout << "Hello World using cout. C" << std::endl;
+}
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(){
+        printf("Hello World using printf. C++\n");
+        std::cout << "Hello World using cout. C++" << std::endl;
+        return 0;
+}
